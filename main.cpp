9a4905a2ed0ed2145e6c3ea24e7892a4b02006e4
Pedro Freire <pedrofreire.freire@gmail.com>
2026-03-14 18:31:36 +0000

    fixed the previous fix and added Read me and documentation

--- main.cpp
+++ main.cpp
@@ -32,15 +32,15 @@ int main() {
 	ResourceManager resourceManager(renderer);
 	Scene scene;
 
-	std::string pathToRoot = "../../";
+	std::string pathToRoot = "../../../";
 	//scene.loadFile(pathToRoot + "scenes/ABeautifulGame/glTF/ABeautifulGame.gltf");
 	//scene.loadFile(pathToRoot + "scenes/Buggy/newBuggy.glb");
 	//scene.loadFile(pathToRoot + "scenes/testWlights.glb");
 	//scene.loadFile(pathToRoot + "scenes/MetalRoughSpheres/glTF/MetalRoughSpheres.gltf");
 	//scene.loadFile(pathToRoot + "scenes/LightsPunctualLamp/glTF-Binary/LightsPunctualLamp.glb");
 	//scene.loadFile(pathToRoot + "scenes/SpecularTest/glTF/SpecularTest.gltf");
-	//scene.loadFile(pathToRoot + "scenes/Sponza/glTF/Sponza.gltf");
-	scene.loadFile(pathToRoot + "scenes/sponzaLight.glb");
+	scene.loadFile(pathToRoot + "scenes/DamagedHelmet/glTF/DamagedHelmet.gltf");
+	//scene.loadFile(pathToRoot + "scenes/sponzaLight.glb");
 	//scene.loadFile(pathToRoot + "scenes/text.glb");
 	resourceManager.loadScene(renderer,scene.getScene());
 	prevTime = timer.now();
